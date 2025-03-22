0a9e78ac75fd9cf91774e7ff37e0df0b8a32c94f
SzLaci04 <laci11nv@gmail.com>
2025-03-22 18:00:15 +0100

    Recepciós osztály létrehozása

---
+++ HotelGuru.DataContext/Entities/Recepcios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuru.DataContext.Entities
+{
+    internal class Recepcios
+    {
+        public void SzamlaKiallitas(Foglalas foglalas) { }
+        public void FoglalasVisszaigazolas(Foglalas foglalas) { }
+        public void VendegCheckIn(Foglalas foglalas, RegisztraltFelhasznalo felhasznalo) { }
+    }
+}
