f0d8f373c02a05b0f04347043c50f446c972827d
lakos14 <csalakos1@gmail.com>
2025-03-22 17:50:16 +0100

    classok

---
+++ HotelGuru.DataContext/Entities/PluszSzolgaltatas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuru.DataContext.Entities
+{
+    internal class PluszSzolgaltatas
+    {
+        public int SzolgaltatasID { get; set; }
+        public string SzolgaltatasNeve { get; set; }
+        public string SzolgaltatasLeiras { get; set; }
+        public int SzolgaltatasAra { get; set; }
+    }
+}
