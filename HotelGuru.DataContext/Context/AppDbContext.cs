a1e79565a2dce69870ea00553f7b5d23dcb34efb
Olivér Praznovszky <praznovszkyoli@gmail.com>
2025-03-23 16:16:49 +0100

    DbContext megirasa

--- HotelGuru.DataContext/Context/AppDbContext.cs
+++ HotelGuru.DataContext/Context/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using HotelGuru.DataContext.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace HotelGuru.DataContext.Context
 {
-    internal class AppDbContext
+    internal class AppDbContext : DbContext
     {
+        public Dbset<Szoba> Szobak { get; set; }
+        public Dbset<Recepcios> Recepciosok { get; set; }
+        public Dbset<PluszSzolgaltatas> PluszSzolgaltatasok { get; set; }
+
+        public Dbset<Felhasznalo> Felhasznalok { get; set; }
+        public Dbset<Foglalas> Foglalasok {  get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
--- Hotelguru/Program.cs
+++ Hotelguru/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Hotelguru.DataContext.Context;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=HotelGuruDB;Trusted_Connection=True;TrustServerCertificate=True;");
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
