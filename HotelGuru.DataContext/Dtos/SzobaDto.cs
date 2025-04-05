c36a601fab35f71023faccd0c70679fb47540037
SzLaci04 <laci11nv@gmail.com>
2025-04-05 18:23:17 +0200

    SzobaDto átalakítása

--- HotelGuru.DataContext/Dtos/SzobaDto.cs
+++ HotelGuru.DataContext/Dtos/SzobaDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,18 @@ namespace HotelGuru.DataContext.Dtos
         public int Id { get; set; } 
         public int AgyakSzama { get; set; } 
         public int EjszakaAr { get; set; } 
+    }
+
+    public class SzobaCreateDto
+    {
+        [Required]
+        public int AgyakSzama { get; set; }
+        [Required]
+        public int EjszakaAr { get; set; }
+    }
+    public class SzobaUpdateDto
+    {
+        public int AgyakSzama { get; set; }
+        public int EjszakaAr { get; set; }
     }
 }
